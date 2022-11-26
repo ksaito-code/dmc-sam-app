29878ec9648ae6bea7f517db8e23c3366c62b4e4
Kota Saito <a2117ks@aiit.ac.jp>
2022-11-26 23:28:34 +0900

    テーブル更新メソッド変更

--- dmc-sam-app/src/post.py
+++ dmc-sam-app/src/post.py
@@ -13,14 +13,29 @@ def lambda_handler(event, context):
   print("exec : post api")
   body = json.loads(event["body"])
 
-  response = daily_moods_table.update_item(
-      Key={'user_id': body['user_id'], 'date': body['date']},
-      UpdateExpression="set mood=:m",
-      ExpressionAttributeValues={
-          ':m': body['mood']},
-      ReturnValues="UPDATED_NEW")
+  user_id = body.get("user_id")
+  date = body.get("date")
+  mood = body.get("mood")
+  main_sleep = body.get("main_sleep")
+  nap_time = body.get("nap_time")
 
-  # print(response)
+  # response = daily_moods_table.update_item(
+  #     Key={'user_id': user_id, 'date': date},
+  #     UpdateExpression="set mood = :m",
+  #     ExpressionAttributeValues={
+  #         ':m': mood
+  #         },
+  #     ReturnValues="UPDATED_NEW")
+  
+  item = {
+    "user_id" : user_id,
+    "date" : date,
+    "mood" : mood,
+    "main_sleep" : main_sleep,
+    "nap_time" : nap_time
+  }
+  
+  daily_moods_table.put_item(Item=item)
   
   return {
       'statusCode': 200,

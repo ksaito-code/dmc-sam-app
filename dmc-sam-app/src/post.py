399f688c105279cbe104d04a0ef9ecbadf1f9448
Kota Saito <a2117ks@aiit.ac.jp>
2022-11-26 15:21:41 +0900

    DailyMoodsテーブルにUpsertする処理へ変更

--- dmc-sam-app/src/post.py
+++ dmc-sam-app/src/post.py
@@ -5,17 +5,23 @@ import boto3
 import uuid
 import json
 
-dynamodb_client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+daily_moods_table = dynamodb.Table('DailyMoods')
 
 def lambda_handler(event, context):
 
   print("exec : post api")
   body = json.loads(event["body"])
 
-  dynamodb_client.put_item(TableName='DmcData', Item={'id': {'S': str(uuid.uuid4())}, \
-                                                      'user_id': {'S': body['user_id']}, \
-                                                      'date': {'S': body['date']}, \
-                                                      'mood': {'S': body['mood']}})
+  response = daily_moods_table.update_item(
+      Key={'user_id': body['user_id'], 'date': body['date']},
+      UpdateExpression="set mood=:m",
+      ExpressionAttributeValues={
+          ':m': body['mood']},
+      ReturnValues="UPDATED_NEW")
+
+  # print(response)
+  
   return {
       'statusCode': 200,
       'body': 'Successfully inserted data!'

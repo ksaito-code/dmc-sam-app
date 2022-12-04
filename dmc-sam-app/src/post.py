26d3802173343b2d1ebd95f3726d67bc7e5b6156
ksaito-code <82599519+ksaito-code@users.noreply.github.com>
2022-12-04 22:38:29 +0900

    睡眠時間を入力可能とする
    DailyMoodsテーブルに睡眠時間を登録する処理を修正

--- dmc-sam-app/src/post.py
+++ dmc-sam-app/src/post.py
@@ -5,17 +5,41 @@ import boto3
 import uuid
 import json
 
-dynamodb_client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
+daily_moods_table = dynamodb.Table('DailyMoods')
 
 def lambda_handler(event, context):
 
   print("exec : post api")
   body = json.loads(event["body"])
 
-  dynamodb_client.put_item(TableName='DmcData', Item={'id': {'S': str(uuid.uuid4())}, \
-                                                      'user_id': {'S': body['user_id']}, \
-                                                      'date': {'S': body['date']}, \
-                                                      'mood': {'S': body['mood']}})
+  user_id = body.get("user_id")
+  date = body.get("date")
+  mood = body.get("mood")
+  sleeps = body.get("sleeps")
+  sleep_minutes = body.get("sleep_minutes")
+  memo = body.get("memo")
+
+
+  # response = daily_moods_table.update_item(
+  #     Key={'user_id': user_id, 'date': date},
+  #     UpdateExpression="set mood = :m",
+  #     ExpressionAttributeValues={
+  #         ':m': mood
+  #         },
+  #     ReturnValues="UPDATED_NEW")
+  
+  item = {
+    "user_id" : user_id,
+    "date" : date,
+    "mood" : mood,
+    "sleeps" : sleeps,
+    "sleep_minutes" : sleep_minutes,
+    "memo" : memo
+  }
+  
+  daily_moods_table.put_item(Item=item)
+  
   return {
       'statusCode': 200,
       'body': 'Successfully inserted data!'
